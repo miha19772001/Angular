922e43fb9fa28424c7f479e7aa69ef98b85d0944
Mikhail <miha.kalachov@gmail.com>
2022-01-16 19:51:53 +0300

    pop-up is done for the third site

--- src/app/components/site-three/game-page/game-page.component.ts
+++ src/app/components/site-three/game-page/game-page.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, BehaviorSubject } from '@reactivex/rxjs';
-import { ElementRef } from '@angular/core';
-import { Renderer2 } from '@angular/core';
-import { Element, ElementAst } from '@angular/compiler';
-import { KeyObject } from 'crypto';
+
 
 @Component({
   selector: 'app-game-page',
@@ -14,10 +10,48 @@ export class GamePageComponent implements OnInit {
 
   public handler: any;
 
-  constructor(private element: ElementRef, private renderer: Renderer2) { }
+  constructor() { }
 
   private speedPlayer = 15;
 
+  public checkPlayer1: number = 0;
+  public checkPlayer2: number = 0;
+
+  private spaceIsPress: boolean = false;
+
+  public startGame(){
+    const buttonStartGame = document.getElementById('button_start_game');
+    const popUp = document.getElementById('pop_up');
+
+    buttonStartGame?.addEventListener('click', (event) => {
+      this.spaceIsPress = !this.spaceIsPress;
+       
+      if (this.spaceIsPress) {
+        popUp?.classList.add('inactive');
+      }
+      else{
+        popUp?.classList.remove('inactive');
+      }
+    });
+  }
+
+  public getPause() {
+
+    const popUp = document.getElementById('pop_up');
+
+    document.addEventListener('keydown', (event) => {
+      if (event.code === 'Space') {
+        this.spaceIsPress = !this.spaceIsPress;
+        event.preventDefault();
+        if (this.spaceIsPress) {
+          popUp?.classList.add('inactive');
+        }
+        else{
+          popUp?.classList.remove('inactive');
+        }
+      }
+    });
+  }
 
   public movePlayer1() {
 
@@ -27,17 +61,17 @@ export class GamePageComponent implements OnInit {
     document.addEventListener('keypress', (event) => {
 
       let player1Top = player1?.getBoundingClientRect().top;
-      let fieldTop = field?.getBoundingClientRect().top;
-
       let player1Bottom = player1?.getBoundingClientRect().bottom;
+
+      let fieldTop = field?.getBoundingClientRect().top;
       let fieldBottom = field?.getBoundingClientRect().bottom;
 
-      if (event.code === 'KeyW' && player1Top! >= fieldTop! + 15) {
+      if (event.code === 'KeyW' && player1Top! >= fieldTop! + 15 && this.spaceIsPress === true) {
         let goTop = player1?.offsetTop;
         player1!.style.top = goTop! - this.speedPlayer + 'px';
       }
 
-      else if (event.code === 'KeyS' && player1Bottom! <= fieldBottom! - 15) {
+      else if (event.code === 'KeyS' && player1Bottom! <= fieldBottom! - 15 && this.spaceIsPress === true) {
         let goTop = player1?.offsetTop;
         player1!.style.top = goTop! + this.speedPlayer + 'px';
       }
@@ -76,13 +110,19 @@ export class GamePageComponent implements OnInit {
       let player2Left = player2!.getBoundingClientRect().left;
       let player2Right = player2!.getBoundingClientRect().right;
 
+      //Счёт первого игрока
+      if (ballLeft + dx <= fieldLeft) this.checkPlayer2 += 1;
+      //Счёт второго игрока
+      if (ballRight + dy >= fieldRight) this.checkPlayer1 += 1;
+
+
       if (ballTop + dy <= fieldTop || ballBottom + dy >= fieldBottom) dy = -dy;
 
       if (ballLeft + dx <= fieldLeft || ballRight + dy >= fieldRight || (ballLeft + dx <= player1Right && (ballTop >= player1Top && ballBottom <= player1Bottom)) || (ballRight + dx >= player2Left && (ballTop >= player2Top && ballBottom <= player2Bottom))) dx = -dx;
 
 
       //Отражение от боков первого игрока
-      if (ballTop + 3 * dy  < player1Bottom && ballBottom + 3 * dy > player1Top && ballRight + 3 * dx <= player1Right) {
+      if (ballTop + 3 * dy < player1Bottom && ballBottom + 3 * dy > player1Top && ballRight + 3 * dx <= player1Right) {
         dx = -dx;
         dy = -dy;
       }
@@ -93,11 +133,14 @@ export class GamePageComponent implements OnInit {
         dy = -dy;
       }
 
-      let goLeft = ball?.offsetLeft;
-      ball!.style.left = goLeft! + dx + 'px';
+      //Остановка мячика при нажатии на Space
+      if (this.spaceIsPress) {
+        let goLeft = ball?.offsetLeft;
+        let goTop = ball?.offsetTop;
 
-      let goTop = ball?.offsetTop;
-      ball!.style.top = goTop! + dy + 'px';
+        ball!.style.left = goLeft! + dx + 'px';
+        ball!.style.top = goTop! + dy + 'px';
+      }
 
     }, 10);
   }
@@ -112,28 +155,29 @@ export class GamePageComponent implements OnInit {
     document.addEventListener('keydown', (event) => {
 
       let player2Top = player2?.getBoundingClientRect().top;
-      let fieldTop = field?.getBoundingClientRect().top;
-
       let player2Bottom = player2?.getBoundingClientRect().bottom;
+
+      let fieldTop = field?.getBoundingClientRect().top;
       let fieldBottom = field?.getBoundingClientRect().bottom;
       //this.unique().find((o:string) => o === 'KeyI')
-      if (event.code === 'KeyI' && player2Top! >= fieldTop! + 15) {
+      if (event.code === 'KeyI' && player2Top! >= fieldTop! + 15 && this.spaceIsPress === true) {
         let goTop = player2?.offsetTop;
         player2!.style.top = goTop! - this.speedPlayer + 'px';
       }
       //this.unique().find((o:string) => o === 'KeyK'
-      else if (event.code === 'KeyK' && player2Bottom! <= fieldBottom! - 15) {
+      else if (event.code === 'KeyK' && player2Bottom! <= fieldBottom! - 15 && this.spaceIsPress === true) {
         let goTop = player2?.offsetTop;
         player2!.style.top = goTop! + this.speedPlayer + 'px';
       }
+
+
     })
   }
   ngOnInit(): void {
+    this.startGame();
     this.movePlayer1();
     this.moveBall();
     this.movePlayer2();
+    this.getPause();
   }
 }
-
-
-
